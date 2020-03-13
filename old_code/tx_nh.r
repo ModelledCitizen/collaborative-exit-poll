6732bed7573353ca9c7cd8420e85a51b3feb5385
UnlikelyVolcano <50756646+UnlikelyVolcano@users.noreply.github.com>
2020-03-13 11:44:01 -0400

    temp for nh and tx

---
+++ old_code/tx_nh.r
@@ -0,0 +1,25 @@
+
+UnlikelyTools::set_wd("collaborative-exit-poll", verbose = T)
+
+
+write.table(
+  paste0(
+    "http://unlikelyvolcano.com/collaborative-exit-poll/scans/2020/nh-p/",
+    list.files("scans/2020/nh-p")
+  ),
+  "mturk-ul/nh-p-1.csv",
+  row.names = F,
+  quote = F,
+  col.names = "PDFURL"
+)
+
+write.table(
+  paste0(
+    "http://unlikelyvolcano.com/collaborative-exit-poll/scans/2020/nh-p/",
+    list.files("scans/2020/tx-p")
+  ),
+  "mturk-ul/tx-p-1.csv",
+  row.names = F,
+  quote = F,
+  col.names = "PDFURL"
+)
